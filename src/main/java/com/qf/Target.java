cb33d26c3f49dd1cb320abe1e14328ccc4c9baf3
huangguizhao <2678383176@qq.com>
2019-10-23 14:20:49 +0800

    Coffee add

--- src/main/java/com/qf/Target.java
+++ src/main/java/com/qf/Target.java
@@ -4,4 +4,7 @@ package com.qf;
  * @author huangguizhao666
  */
 public class Target {
+    public static void main(String[] args){
+        System.out.println("CoffeeAndLife daociyiyou!");
+    }
 }
